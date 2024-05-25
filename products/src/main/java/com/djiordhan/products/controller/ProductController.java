def008b7e1417a12bbd3334c15033d7e55b73175
jordan <jordandiamante@icloud.com>
2024-05-25 13:35:10 +0800

    autowire restTemplate, fetch stocks from different service

--- products/src/main/java/com/djiordhan/products/controller/ProductController.java
+++ products/src/main/java/com/djiordhan/products/controller/ProductController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.djiordhan.products.model.Product;
 import com.djiordhan.products.repsonse.ProductResponse;
+import com.djiordhan.products.repsonse.StockResponse;
 import com.djiordhan.products.service.ProductService;
 
 @RestController
@@ -21,14 +23,24 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping
     public List<Product> findAll() {
         return productService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Optional<ProductResponse> findById(@PathVariable Long id) {
-        return productService.findById(id);
+    public ProductResponse findById(@PathVariable Long id) {
+
+        ProductResponse productResponse = productService.findById(id).get();
+
+        StockResponse stock = restTemplate.getForObject("http://localhost:3000/stocks/{id}", StockResponse.class, id);
+
+        productResponse.setStock(stock);
+
+        return productResponse;
     }
 
     @GetMapping("/withStocks/{id}")
