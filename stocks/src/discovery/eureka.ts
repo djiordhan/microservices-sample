deb7f11b131bf8c1ab968940b8939ec291254e98
jordan <jordandiamante@icloud.com>
2024-05-29 16:15:51 +0800

    connect nodejs to eureka

---
+++ stocks/src/discovery/eureka.ts
@@ -0,0 +1,39 @@
+import { Eureka } from 'eureka-js-client';
+
+export const registerWithEureka = (port: number) => {
+    const client = new Eureka({
+        instance: {
+            app: 'stocks',
+            hostName: 'localhost',
+            ipAddr: '127.0.0.1',
+            statusPageUrl: `http://localhost:${port}/stocks/1`,
+            port: {
+                '$': port,
+                '@enabled': true,
+            },
+            vipAddress: 'stocks',
+            dataCenterInfo: {
+                '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+                name: 'MyOwn',
+            },
+        },
+        eureka: {
+            host: 'localhost',
+            port: 8761,
+            servicePath: '/eureka/apps/',
+        },
+    });
+
+    client.start((error) => {
+        if (error) {
+            console.error('Error starting Eureka client:', error);
+        } else {
+            console.log('Eureka client started successfully');
+        }
+    });
+
+    // Optionally, handle the shutdown of the client gracefully
+    process.on('SIGINT', () => {
+        client.stop();
+    });
+};
--- stocks/src/index.ts
+++ stocks/src/index.ts
@@ -1,9 +1,11 @@
-import express from 'express';
-import stockRoutes from './routes/stockRoutes';
 import bodyParser from 'body-parser';
+import express from 'express';
+import { registerWithEureka } from './discovery/eureka';
+import stockRoutes from './routes/stockRoutes';
+
+const port = process.env.PORT || 3000;
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json()); // Parse JSON bodies
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,5 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/stocks', stockRoutes);
 
 app.listen(port, () => {
+  registerWithEureka(+port);
   console.log(`Server is running at http://localhost:${port}`);
 });
