14c807a7950056e925ba5e6f0bdc2c96a526b47f
jordan <jordandiamante@icloud.com>
2024-06-06 21:46:29 +0800

    update docker files

--- stocks/docker-run.sh
+++ stocks/docker-run.sh
@@ -1,8 +1,8 @@
 docker run -p 8085:8085 \
-    -e DB_URL=jdbc:mysql://localhost:3306/stocks \
+    -e DB_URL=jdbc:mysql://localhost:3307/stocks \
     -e DB_USER=root \
     -e DB_PASSWORD=root \
-    -e SERVER_PORT=8086 \
-    -e CONTEXT_PATH=/members \
+    -e SERVER_PORT=8088 \
+    -e CONTEXT_PATH=/stocks \
     -e EUREKA_SERVER=http://localhost:8761/eureka/ \
     stocks-service
